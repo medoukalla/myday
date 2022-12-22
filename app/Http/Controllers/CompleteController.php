cfaeb1e8c82dc680e639874c8a4e6087f838d4fa
medoukalla <medoukalla@gmail.com>
2022-12-22 15:48:13 +0100

    Add complte model files

---
+++ app/Http/Controllers/CompleteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Complete;
+use Illuminate\Http\Request;
+
+class CompleteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Complete  $complete
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Complete $complete)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Complete  $complete
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Complete $complete)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Complete  $complete
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Complete $complete)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Complete  $complete
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Complete $complete)
+    {
+        //
+    }
+}
