455f10f607a537e9dbc07aafc0522cdcafa09bcd
medoukalla <medoukalla@gmail.com>
2022-12-22 13:41:24 +0100

    Create tasks page in dashboard and add some styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,11 +2,124 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <link rel="stylesheet" href="{{ asset('build/assets/styles.css') }}">
+                <div class="container">
+                    <div class="title-wrap-centre">
+                        <h2>لائحة المهام</h2>
+                    </div>
 
-                    {{ __("Welcome") }}
+                    <div class="w-layout-grid works-grid">
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6df6cd09264_Step%2001.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Adapt your menu items</h5>
+                                <p>Easily adapt your menu using the webflow CMS and allow customers to browse your
+                                    items.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee60a21d09267_Step%2002.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Accept online orders &amp; takeout</h5>
+                                <p>Let your customers order and pay via the powerful ecommerce system, or simple let
+                                    them call your store.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6fdf0d0926a_Step%2003.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Manage delivery or takeout</h5>
+                                <p>Manage your own logistics and take orders simply through the ecommerce system.</p>
+                            </div>
+                        </div>
+
+                        
+                    </div>
+
+                    <div class="w-layout-grid works-grid">
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6df6cd09264_Step%2001.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Adapt your menu items</h5>
+                                <p>Easily adapt your menu using the webflow CMS and allow customers to browse your
+                                    items.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee60a21d09267_Step%2002.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Accept online orders &amp; takeout</h5>
+                                <p>Let your customers order and pay via the powerful ecommerce system, or simple let
+                                    them call your store.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6fdf0d0926a_Step%2003.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Manage delivery or takeout</h5>
+                                <p>Manage your own logistics and take orders simply through the ecommerce system.</p>
+                            </div>
+                        </div>
+
+                        
+                    </div>
+
+                    <div class="w-layout-grid works-grid">
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6df6cd09264_Step%2001.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Adapt your menu items</h5>
+                                <p>Easily adapt your menu using the webflow CMS and allow customers to browse your
+                                    items.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee60a21d09267_Step%2002.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Accept online orders &amp; takeout</h5>
+                                <p>Let your customers order and pay via the powerful ecommerce system, or simple let
+                                    them call your store.</p>
+                            </div>
+                        </div>
+                        <div class="line-column">
+                            <div class="line"></div>
+                        </div>
+                        <div class="content-card"><img
+                                src="https://uploads-ssl.webflow.com/63a44bdc311ee6b658d091fc/63a44bdc311ee6fdf0d0926a_Step%2003.png"
+                                alt="" class="step-image">
+                            <div class="content-wrapper">
+                                <h5>Manage delivery or takeout</h5>
+                                <p>Manage your own logistics and take orders simply through the ecommerce system.</p>
+                            </div>
+                        </div>
+
+                        
+                    </div>
                 </div>
+
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
