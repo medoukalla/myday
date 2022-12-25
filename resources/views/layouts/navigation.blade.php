87741c8fc7c3648ad9a864d1c6c3f526f1184305
medoukalla <medoukalla@gmail.com>
2022-12-25 20:49:37 +0100

    Fix the logout button and change welcome page content

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -79,15 +79,15 @@
                 {{ __('Profile') }}
             </x-responsive-nav-link>
 
-            <x-responsive-nav-link :href="route('logout')"
-                onclick="event.preventDefault();
-                this.closest('form').submit();">
-                {{ __('Log Out') }}
-            </x-responsive-nav-link>
-
             <!-- Authentication -->
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                        onclick="event.preventDefault();
+                            this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                </x-dropdown-link>
             </form>
         </div>
 
