5d11293753de14e987aa44349d69200815042428
medoukalla <medoukalla@gmail.com>
2023-05-27 22:27:56 +0100

    Install livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="stylesheet" href="{{ asset('build/assets/styles.css') }}">
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -36,5 +37,8 @@
                 {{ $slot }}
             </main>
         </div>
+
+
+        @livewireScripts
     </body>
 </html>
